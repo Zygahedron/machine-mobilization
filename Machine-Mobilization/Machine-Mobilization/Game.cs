327deeac82e83e61adb53c1573c514406b13f8ea
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2023-01-07 16:05:42 -0500

    Create Game.cs

---
+++ Machine-Mobilization/Machine-Mobilization/Game.cs
@@ -0,0 +1,20 @@
+﻿
+using SFML.Graphics;
+using SFML.Window;
+using System.Collections.Generic;
+
+namespace Machine_Mobilization;
+
+internal static class Game
+{
+	public static RenderWindow Window { get; } =
+		new(new VideoMode(800, 600), "Machine Mobilization", Styles.Close | Styles.Titlebar);
+
+	public static List<Player> Players = new(8);
+
+	public static void Close()
+	{
+		Window.Close();
+	}
+}
+
