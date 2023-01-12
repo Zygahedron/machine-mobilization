399ae5ed6f60b78a2e04ab4f7e7ebb7edc4f0f38
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2023-01-12 00:11:41 -0500

    Create AssetManager.cs

---
+++ Machine-Mobilization/Machine-Mobilization/AssetManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace Machine_Mobilization;
+
+internal class AssetManager
+{
+	public Texture? GetTexture(string name) =>
+		this._textures.TryGetValue(name, out Texture? _) ? this._textures[name] : null;
+
+	public Texture? LoadTexture(string name, string filename)
+	{
+		var texture = new Texture(filename);
+
+		if (this._textures.ContainsKey(name))
+			this._textures[name] = texture;
+		else
+			this._textures.Add(name, texture);
+
+		return this._textures[name];
+	}
+
+	public void UnloadTexture(string name) => this._textures.Remove(name);
+	public void ClearTextures()            => this._textures.Clear();
+
+
+	private readonly Dictionary<string, Texture> _textures = new();
+}
