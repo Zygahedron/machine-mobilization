416c01156337be3a1109243cf6f3db9706058b62
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2023-01-12 18:15:18 -0500

    Create IMouseInteractable.cs

---
+++ Machine-Mobilization/Machine-Mobilization/IMouseInteractable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Machine_Mobilization;
+
+internal interface IMouseInteractable
+{
+	public IntRect BoundingBox { get; set; }
+
+	public bool IsMouseOver(int mouseX, int mouseY) => this.BoundingBox.Contains(mouseX, mouseY);
+
+	public void OnMouseEnter()                   { }
+	public void OnMouseHover(TimeSpan deltaTime) { }
+	public void OnMouseExit()                    { }
+
+	public void OnMouseButtonDown(Mouse.Button button)                     { }
+	public void OnMouseButtonHold(Mouse.Button button, TimeSpan deltaTime) { }
+	public void OnMouseButtonUp(Mouse.Button button)                       { }
+
+	public void OnMouseScroll(float deltaScroll) { }
+}
