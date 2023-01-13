9e82685958155c6420048efb63d69735d8049818
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2023-01-12 21:34:40 -0500

    Change Game.Players to Game.Robots

--- Machine-Mobilization/Machine-Mobilization/Game.cs
+++ Machine-Mobilization/Machine-Mobilization/Game.cs
@@ -14,7 +14,7 @@ internal static class Game
 	public static RenderWindow Window { get; } =
 		new(new VideoMode(Resolution.X, Resolution.Y), "Machine Mobilization", Styles.Close | Styles.Titlebar);
 
-	public static List<Robot> Players = new(8);
+	public static List<Robot> Robots { get; set; } = new(8);
 
 	public static void Close()
 	{
